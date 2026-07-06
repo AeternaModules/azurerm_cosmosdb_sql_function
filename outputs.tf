d1e4745e833e729e8af131030e000f0684dbcb7f
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:11:54 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,16 @@ output "cosmosdb_sql_functions" {
   description = "All cosmosdb_sql_function resources"
   value       = azurerm_cosmosdb_sql_function.cosmosdb_sql_functions
 }
+output "cosmosdb_sql_functions_body" {
+  description = "List of body values across all cosmosdb_sql_functions"
+  value       = [for k, v in azurerm_cosmosdb_sql_function.cosmosdb_sql_functions : v.body]
+}
+output "cosmosdb_sql_functions_container_id" {
+  description = "List of container_id values across all cosmosdb_sql_functions"
+  value       = [for k, v in azurerm_cosmosdb_sql_function.cosmosdb_sql_functions : v.container_id]
+}
+output "cosmosdb_sql_functions_name" {
+  description = "List of name values across all cosmosdb_sql_functions"
+  value       = [for k, v in azurerm_cosmosdb_sql_function.cosmosdb_sql_functions : v.name]
+}
 
