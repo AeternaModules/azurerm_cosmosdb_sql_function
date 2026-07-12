921a6bd05931f9813b43d17029a9482f016da709
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:01:19 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "cosmosdb_sql_functions_id" {
+  description = "Map of id values across all cosmosdb_sql_functions, keyed the same as var.cosmosdb_sql_functions"
+  value       = { for k, v in azurerm_cosmosdb_sql_function.cosmosdb_sql_functions : k => v.id }
+}
 output "cosmosdb_sql_functions_body" {
   description = "Map of body values across all cosmosdb_sql_functions, keyed the same as var.cosmosdb_sql_functions"
   value       = { for k, v in azurerm_cosmosdb_sql_function.cosmosdb_sql_functions : k => v.body }
