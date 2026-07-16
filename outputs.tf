fdb44f43314a30ad9180074a3dfaf23a5b8c4ee3
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:47:12 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,17 @@
 output "cosmosdb_sql_functions_id" {
   description = "Map of id values across all cosmosdb_sql_functions, keyed the same as var.cosmosdb_sql_functions"
-  value       = { for k, v in azurerm_cosmosdb_sql_function.cosmosdb_sql_functions : k => v.id }
+  value       = { for k, v in azurerm_cosmosdb_sql_function.cosmosdb_sql_functions : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "cosmosdb_sql_functions_body" {
   description = "Map of body values across all cosmosdb_sql_functions, keyed the same as var.cosmosdb_sql_functions"
-  value       = { for k, v in azurerm_cosmosdb_sql_function.cosmosdb_sql_functions : k => v.body }
+  value       = { for k, v in azurerm_cosmosdb_sql_function.cosmosdb_sql_functions : k => v.body if v.body != null && length(v.body) > 0 }
 }
 output "cosmosdb_sql_functions_container_id" {
   description = "Map of container_id values across all cosmosdb_sql_functions, keyed the same as var.cosmosdb_sql_functions"
-  value       = { for k, v in azurerm_cosmosdb_sql_function.cosmosdb_sql_functions : k => v.container_id }
+  value       = { for k, v in azurerm_cosmosdb_sql_function.cosmosdb_sql_functions : k => v.container_id if v.container_id != null && length(v.container_id) > 0 }
 }
 output "cosmosdb_sql_functions_name" {
   description = "Map of name values across all cosmosdb_sql_functions, keyed the same as var.cosmosdb_sql_functions"
-  value       = { for k, v in azurerm_cosmosdb_sql_function.cosmosdb_sql_functions : k => v.name }
+  value       = { for k, v in azurerm_cosmosdb_sql_function.cosmosdb_sql_functions : k => v.name if v.name != null && length(v.name) > 0 }
 }
 
